6b5ea19d338d421813c1b84c721ffec70f526732
Christopher Jacobs <cjacobs@drobo.com>
2017-11-29 11:21:24 -0800

    Turn some text into numbers!

--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@ program
     .version(pkg.version)
     .arguments('<dir> [folders] [depth] [files] [streams] [size] [bs]')
     .action((dir, folders = 20, depth = 2, files = 1000, streams = 8, size = '20MB', bs = '2MB') => {
-        size = bytes.parse(size);
-        bs   = bytes.parse(bs);
+        size    = bytes.parse(size);
+        bs      = bytes.parse(bs);
+        folders = Number(folders)
+        depth   = Number(depth)
+        files   = Number(files)
+        streams = Number(streams)
+
         writer(dir, folders, depth, files, streams, size, bs, err => {
             if (err)
                 console.error(err)
